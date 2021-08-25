201d2879b0cb679f449360bc59c54741991b4186
WhiteGrouse <39170892+WhiteGrouse@users.noreply.github.com>
2021-08-26 00:50:04 +0900

    MVVMへ

--- JellyParfait/Data/MusicData.cs
+++ JellyParfait/Data/MusicData.cs
@@ -6,12 +6,6 @@ namespace JellyParfait.Data {
 
     public class MusicData {
 
-        private MainWindow main;
-
-        public MusicData(MainWindow main) {
-            this.main = main;
-        }
-
         public int QuereId { get; set; }
 
         public string Title { get; set; }
@@ -22,55 +16,8 @@ namespace JellyParfait.Data {
 
         public string YoutubeUrl { get; set; }
 
-        public string Thumbnails { get; set; }
-
         public Visibility Visibility { get; set; }
 
         public string Color { get; set; }
-
-        public RelayCommand ClickCommand {
-            get {
-                return new RelayCommand(() => {
-                    if (!main.getClickedFlag()) {
-                        if (main.getQuereId() != QuereId) {
-                            main.SetQuere(QuereId);
-                        } else {
-                            main.changeClickedFlag(true);
-                            (main.IsPlay() ? (Action)main.Pause : main.Play)();
-                            main.changeClickedFlag(false);
-                        }
-                    }
-                });
-            }
-        }
-
-        public RelayCommand UpCommand {
-            get {
-                return new RelayCommand(() => {
-                    if (!main.getClickedFlag()) {
-                        main.UpMusic(QuereId);
-                    }
-                });
-            }
-        }
-
-        public RelayCommand DownCommand {
-            get {
-                return new RelayCommand(() => {
-                    if (!main.getClickedFlag()) {
-                        main.DownMusic(QuereId);
-                    }
-                });
-            }
-        }
-        public RelayCommand DisposeCommand {
-            get {
-                return new RelayCommand(() => {
-                    if (!main.getClickedFlag()) {
-                        main.DisposeMusicFromQuere(QuereId);
-                    }
-                });
-            }
-        }
     }
 }
---
+++ JellyParfait/MVVM/ViewModel/IPlayer.cs
@@ -0,0 +1,30 @@
+﻿using JellyParfait.MVVM.Model;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JellyParfait.MVVM.ViewModel
+{
+    public interface IPlayer
+    {
+        TimeSpan Current { get; }
+        TimeSpan Total { get; }
+        int TotalSec { get; }
+        int Elapsed { get; }
+        bool Loop { get; set; }
+        bool Shuffle { get; set; }
+        bool WithPlaylist { get; set; }
+        bool IsPlaying { get; }
+        Music Music { get; set; }
+
+        event EventHandler<StoppedEventArgs> PlayerStopped;
+
+        void Play();
+        void Pause();
+        void Stop();
+    }
+}
