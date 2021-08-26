15b152bdf5ec45a71e69cbb298a939feef0ab38e
WhiteGrouse <39170892+WhiteGrouse@users.noreply.github.com>
2021-08-26 17:27:10 +0900

    fix typo(TimeSpanConverter)

--- JellyParfait/Converter/TimeSpanConverter.cs
+++ JellyParfait/Converter/TimeSpanConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 namespace JellyParfait.Converter
 {
     [ValueConversion(typeof(TimeSpan), typeof(string))]
-    public class TiemSpanConverter : IValueConverter
+    public class TimeSpanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
